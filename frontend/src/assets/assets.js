575dbde58108227cb98d527e94580e5b360f8215
Hsin-arp <pranish909@gmail.com>
2025-03-23 08:14:41 +0545

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,24 @@
+import logo from './logo.png'
+import bluejeans from './bluejeans.png'
+
+export const assets = {
+    logo,
+    bluejeans
+}
+
+export const products = [
+    {
+        _id: "aa",
+        name: "Jeans",
+        price: 1200,
+        image: [bluejeans],
+        category: "Women",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+
+    }
+
+]
+    
